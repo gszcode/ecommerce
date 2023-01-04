6f57b79d9906a3e06c7c929f1969d46e446623a4
Gabriel <gszcode@gmail.com>
2023-01-04 04:05:40 -0300

    verificar token y rol de usuario

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,36 @@
+const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+const asyncHandler = require('express-async-handler')
+
+const authMiddleware = asyncHandler(async (req, res, next) => {
+  let token = req?.headers?.authorization.split(' ')[1]
+
+  if (!token) {
+    throw new Error('There is no token attached to header')
+  }
+
+  try {
+    const { id } = jwt.verify(token, process.env.JWT_SECRET)
+
+    const user = await User.findById(id)
+    req.user = user
+
+    next()
+  } catch (error) {
+    throw new Error('Not Authorized token expired, Please Login again')
+  }
+})
+
+const isAdmin = asyncHandler(async (req, res, next) => {
+  // const {email} = req.user
+  // const adminUser = await User.findOne({ email })
+  const { role } = req.user
+
+  if (role !== 'admin') {
+    throw new Error('You are not an admin')
+  }
+
+  next()
+})
+
+module.exports = { authMiddleware, isAdmin }
