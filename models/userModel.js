8d4b568dff5bf5b519ff792504911b22ae1d033b
Gabriel <gszcode@gmail.com>
2023-01-04 00:29:33 -0300

    creado modelos

---
+++ models/userModel.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const userSchema = new Schema({
+  firstname: {
+    type: String,
+    required: true,
+    unique: true,
+    index: true
+  },
+  lastname: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  mobile: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    default: 'user'
+  }
+})
+
+userSchema.pre('save', async function (next) {
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+
+  next()
+})
+
+userSchema.methods.isPasswordMatched = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password)
+}
+
+module.exports = model('User', userSchema)
