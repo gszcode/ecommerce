b5c9228dd98fce31dc96fd45aa8a6cc112441296
Gabriel <gszcode@gmail.com>
2023-01-04 04:03:37 -0300

    agregado nuevas caracteristicas

--- models/userModel.js
+++ models/userModel.js
@@ -1,36 +1,61 @@
-const { Schema, model } = require('mongoose')
+const { Schema, model, isObjectIdOrHexString } = require('mongoose')
 const bcrypt = require('bcrypt')
 
-const userSchema = new Schema({
-  firstname: {
-    type: String,
-    required: true,
-    unique: true,
-    index: true
+const userSchema = new Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true
+    },
+    lastname: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    mobile: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    role: {
+      type: String,
+      default: 'user'
+    },
+    isBlocked: {
+      type: Boolean,
+      default: false
+    },
+    cart: {
+      type: Array,
+      default: []
+    },
+    address: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Addres'
+      }
+    ],
+    wishList: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Product'
+      }
+    ]
   },
-  lastname: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  mobile: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  role: {
-    type: String,
-    default: 'user'
+  {
+    timestamps: true
   }
-})
+)
 
 userSchema.pre('save', async function (next) {
   const salt = await bcrypt.genSalt(10)
