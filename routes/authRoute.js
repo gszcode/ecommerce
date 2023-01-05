27b73330003276a1fb3ccad6bbf14480070d4cb0
Gabriel <gszcode@gmail.com>
2023-01-05 02:24:33 -0300

    agregadas nuevas funcionalidades y dependencias

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const { authMiddleware, isAdmin } = require('../middlewares/authMiddleware')
 const {
   createUser,
   loginUser,
@@ -8,13 +9,16 @@ const {
   updateUser,
   deleteUser,
   blockUser,
-  unblockUser
+  unblockUser,
+  handleRefreshToken,
+  logoutUser
 } = require('../controllers/userCtrl')
-const { authMiddleware, isAdmin } = require('../middlewares/authMiddleware')
 
 router.post('/register', createUser)
 router.post('/login', loginUser)
+router.get('/logout', logoutUser)
 router.get('/all-users', getUsers)
+router.get('/refresh', handleRefreshToken)
 router.get('/:id', authMiddleware, isAdmin, getUser)
 router.put('/edit-user/:id', authMiddleware, updateUser)
 router.delete('/:id', deleteUser)
