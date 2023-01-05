0fb02623527b26459b19a98520bff0ab1b0d9386
Gabriel <gszcode@gmail.com>
2023-01-05 02:21:46 -0300

    agregador middlewares

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 require('dotenv').config()
+const cookieParser = require('cookie-parser')
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 4000
+const morgan = require('morgan')
 
 // Connection to DB
 const dbConnect = require('./config/dbConnect')
 const { notFound, errorHandler } = require('./middlewares/errorHandler')
 dbConnect()
 
-// // Middlewares
+// Middlewares
 app.use(express.json())
+app.use(cookieParser())
+app.use(morgan('dev'))
 
 // Routes
 const authRouter = require('./routes/authRoute')
+const productRouter = require('./routes/productRoute')
 app.use('/api/user', authRouter)
+app.use('/api/product', productRouter)
 
 // Error handler
 app.use(notFound)
