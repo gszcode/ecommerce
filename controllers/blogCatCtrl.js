31c943d9edc58561d796f8cc377a82b3ad5f82a8
Gabriel <gszcode@gmail.com>
2023-01-23 02:41:39 -0300

    agregado controlador: crear, editar, eliminar, obtener una marca y obtener todas

---
+++ controllers/blogCatCtrl.js
@@ -0,0 +1,71 @@
+const Category = require('../models/blogCatModel')
+const asyncHandler = require('express-async-handler')
+const validateMongoDbId = require('../utils/validateMongodbId')
+
+// Create category
+const createCategory = asyncHandler(async (req, res) => {
+  try {
+    const newCategory = await Category.create(req.body)
+
+    res.json(newCategory)
+  } catch (error) {
+    throw new Error(error)
+  }
+})
+
+// Update category
+const updateCategory = asyncHandler(async (req, res) => {
+  const { id } = req.params
+  try {
+    const updateCategory = await Category.findByIdAndUpdate(id, req.body, {
+      new: true
+    })
+
+    res.json(updateCategory)
+  } catch (error) {
+    throw new Error(error)
+  }
+})
+
+// Delete category
+const deleteCategory = asyncHandler(async (req, res) => {
+  const { id } = req.params
+  try {
+    const deleteCategory = await Category.findByIdAndDelete(id)
+
+    res.json(deleteCategory)
+  } catch (error) {
+    throw new Error(error)
+  }
+})
+
+// Get category
+const getCategory = asyncHandler(async (req, res) => {
+  const { id } = req.params
+  try {
+    const getACategory = await Category.findById(id)
+
+    res.json(getACategory)
+  } catch (error) {
+    throw new Error(error)
+  }
+})
+
+// Get all category
+const getAllCategory = asyncHandler(async (req, res) => {
+  try {
+    const getCategorys = await Category.find()
+
+    res.json(getCategorys)
+  } catch (error) {
+    throw new Error(error)
+  }
+})
+
+module.exports = {
+  createCategory,
+  updateCategory,
+  deleteCategory,
+  getCategory,
+  getAllCategory
+}
