100ed3d16630fe4b8d11030875be465438d0cfac
Gabriel <gszcode@gmail.com>
2023-03-08 00:30:52 -0300

    componente de blog/:id creado

---
+++ client/src/pages/SingleBlog.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import BreadCrumb from '../components/BreadCrumb'
+import Meta from '../components/Meta'
+import { HiOutlineArrowLeft } from 'react-icons/hi'
+
+function SingleBlog() {
+  return (
+    <>
+      <Meta title="Dynamic Blog Name" />
+      <BreadCrumb title="Dynamic Blog Name" />
+      <div className="blog-wrapper home-wrapper-2 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="single-blog-card">
+                <Link to="/blogs" className="d-flex align-items-center gap-10">
+                  <HiOutlineArrowLeft className="fs-4" /> Go Back To Blogs
+                </Link>
+                <h3 className="title">
+                  A Beautiful Sunday Morning Renaissance
+                </h3>
+                <img
+                  src="images/blog-1.jpg"
+                  className="img-fluid w-100 my-4"
+                  alt="blog"
+                />
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book. It has survived not only five centuries, but
+                  also the leap into electronic typesetting, remaining
+                  essentially unchanged.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default SingleBlog
