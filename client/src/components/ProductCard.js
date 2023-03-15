9b0ca592f084ea45b50d18e3a3f829209277fb1c
Gabriel <gszcode@gmail.com>
2023-03-15 06:34:54 -0300

    finalizado front sin conexion a api

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -18,11 +18,20 @@ function ProductCard({ grid }) {
           location.pathname === '/product' ? `gr-${grid}` : 'col-3'
         }`}
       >
-        <Link to=":id" className="product-card position-relative">
+        <Link
+          to={`${
+            location.pathname === '/'
+              ? '/product/:id'
+              : location.pathname === '/product/:id'
+              ? '/product/:id'
+              : ':id'
+          }`}
+          className="product-card position-relative"
+        >
           <div className="wishlist-icon position-absolute">
-            <Link to="/">
+            <button className="border-0 bg-transparent">
               <img src={wish} alt="wishlist" />
-            </Link>
+            </button>
           </div>
           <div className="product-image">
             <img src={watch} alt="product" className="img-fluid" />
@@ -50,15 +59,74 @@ function ProductCard({ grid }) {
           </div>
           <div className="action-bar position-absolute">
             <div className="d-flex flex-column gap-15">
-              <Link to="/">
+              <button className="border-0 bg-transparent">
                 <img src={prodcompare} alt="compare" />
-              </Link>
-              <Link to="/">
+              </button>
+              <button className="border-0 bg-transparent">
                 <img src={view} alt="view" />
-              </Link>
-              <Link to="/">
+              </button>
+              <button className="border-0 bg-transparent">
                 <img src={cart} alt="addcart" />
-              </Link>
+              </button>
+            </div>
+          </div>
+        </Link>
+      </div>
+      <div
+        className={`${
+          location.pathname === '/product' ? `gr-${grid}` : 'col-3'
+        }`}
+      >
+        <Link
+          to={`${
+            location.pathname === '/'
+              ? '/product/:id'
+              : location.pathname === '/product/:id'
+              ? '/product/:id'
+              : ':id'
+          }`}
+          className="product-card position-relative"
+        >
+          <div className="wishlist-icon position-absolute">
+            <button className="border-0 bg-transparent">
+              <img src={wish} alt="wishlist" />
+            </button>
+          </div>
+          <div className="product-image">
+            <img src={watch} alt="product" className="img-fluid" />
+            <img src={watch_1} alt="product" className="img-fluid" />
+          </div>
+          <div className="product-details">
+            <h6 className="brand">Havels</h6>
+            <h5 className="product-title">
+              Kids headphones bulk 10 pack multi colored for students
+            </h5>
+            <ReactStars
+              count={5}
+              value={3}
+              edit={false}
+              size={24}
+              activeColor="#ffd700"
+            />
+            <p className={`description ${grid === 12 ? 'd-block' : 'd-none'}`}>
+              At vero eos et accusamus et iusto osio dignissimos ducimis qui
+              blanditiis prasentium voluptatum deleniti atque currupti quos
+              dolores e quas molestias exceptuti sint occaecati cupiditate nin
+              provident, similique sunt...
+            </p>
+            <p className="price">$100.00</p>
+          </div>
+          <div className="action-bar position-absolute">
+            <div className="d-flex flex-column gap-15">
+              <button className="border-0 bg-transparent">
+                <img src={prodcompare} alt="compare" />
+              </button>
+              <button className="border-0 bg-transparent">
+                <img src={view} alt="view" />
+              </button>
+              <button className="border-0 bg-transparent">
+                <img src={cart} alt="addcart" />
+              </button>
             </div>
           </div>
         </Link>
@@ -70,9 +138,9 @@ function ProductCard({ grid }) {
       >
         <Link className="product-card position-relative">
           <div className="wishlist-icon position-absolute">
-            <Link to="/">
+            <button className="border-0 bg-transparent">
               <img src={wish} alt="wishlist" />
-            </Link>
+            </button>
           </div>
           <div className="product-image">
             <img src={watch} alt="product" className="img-fluid" />
@@ -100,15 +168,15 @@ function ProductCard({ grid }) {
           </div>
           <div className="action-bar position-absolute">
             <div className="d-flex flex-column gap-15">
-              <Link to="/">
+              <button className="border-0 bg-transparent">
                 <img src={prodcompare} alt="compare" />
-              </Link>
-              <Link to="/">
+              </button>
+              <button className="border-0 bg-transparent">
                 <img src={view} alt="view" />
-              </Link>
-              <Link to="/">
+              </button>
+              <button className="border-0 bg-transparent">
                 <img src={cart} alt="addcart" />
-              </Link>
+              </button>
             </div>
           </div>
         </Link>
@@ -120,9 +188,9 @@ function ProductCard({ grid }) {
       >
         <Link className="product-card position-relative">
           <div className="wishlist-icon position-absolute">
-            <Link to="/">
+            <button className="border-0 bg-transparent">
               <img src={wish} alt="wishlist" />
-            </Link>
+            </button>
           </div>
           <div className="product-image">
             <img src={watch} alt="product" className="img-fluid" />
@@ -150,65 +218,15 @@ function ProductCard({ grid }) {
           </div>
           <div className="action-bar position-absolute">
             <div className="d-flex flex-column gap-15">
-              <Link to="/">
+              <button className="border-0 bg-transparent">
                 <img src={prodcompare} alt="compare" />
-              </Link>
-              <Link to="/">
+              </button>
+              <button className="border-0 bg-transparent">
                 <img src={view} alt="view" />
-              </Link>
-              <Link to="/">
+              </button>
+              <button className="border-0 bg-transparent">
                 <img src={cart} alt="addcart" />
-              </Link>
-            </div>
-          </div>
-        </Link>
-      </div>
-      <div
-        className={`${
-          location.pathname === '/product' ? `gr-${grid}` : 'col-3'
-        }`}
-      >
-        <Link className="product-card position-relative">
-          <div className="wishlist-icon position-absolute">
-            <Link to="/">
-              <img src={wish} alt="wishlist" />
-            </Link>
-          </div>
-          <div className="product-image">
-            <img src={watch} alt="product" className="img-fluid" />
-            <img src={watch_1} alt="product" className="img-fluid" />
-          </div>
-          <div className="product-details">
-            <h6 className="brand">Havels</h6>
-            <h5 className="product-title">
-              Kids headphones bulk 10 pack multi colored for students
-            </h5>
-            <ReactStars
-              count={5}
-              value={3}
-              edit={false}
-              size={24}
-              activeColor="#ffd700"
-            />
-            <p className={`description ${grid === 12 ? 'd-block' : 'd-none'}`}>
-              At vero eos et accusamus et iusto osio dignissimos ducimis qui
-              blanditiis prasentium voluptatum deleniti atque currupti quos
-              dolores e quas molestias exceptuti sint occaecati cupiditate nin
-              provident, similique sunt...
-            </p>
-            <p className="price">$100.00</p>
-          </div>
-          <div className="action-bar position-absolute">
-            <div className="d-flex flex-column gap-15">
-              <Link to="/">
-                <img src={prodcompare} alt="compare" />
-              </Link>
-              <Link to="/">
-                <img src={view} alt="view" />
-              </Link>
-              <Link to="/">
-                <img src={cart} alt="addcart" />
-              </Link>
+              </button>
             </div>
           </div>
         </Link>
