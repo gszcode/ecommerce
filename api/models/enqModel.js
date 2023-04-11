c6a2d1c25f38c3e6f61ce8a99a5ff8ad177d1e83
Gabriel <gszcode@gmail.com>
2023-04-11 20:19:01 -0300

    modelo de consultas

---
+++ api/models/enqModel.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require('mongoose')
+
+const enqSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    mobile: {
+      type: String,
+      required: true
+    },
+    comment: {
+      type: String,
+      required: true
+    },
+    status: {
+      type: String,
+      default: 'Submitted',
+      enum: ['Submitted', 'Contacted', 'In Progress']
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+module.exports = model('Enquiry', enqSchema)
