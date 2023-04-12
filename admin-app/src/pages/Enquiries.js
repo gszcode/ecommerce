436107836a0a661f0a72b2bfa6c0eee00f4d07d0
Gabriel <gszcode@gmail.com>
2023-04-12 18:28:15 -0300

    renderizado de datos en pagina de enquiry/consultas

--- admin-app/src/pages/Enquiries.js
+++ admin-app/src/pages/Enquiries.js
@@ -1,5 +1,9 @@
-import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Table } from 'antd'
+import { useEffect } from 'react'
+import { getEnquiries } from '../features/enquiry/enquirySlice'
+import { AiFillDelete } from 'react-icons/ai'
+import { Link } from 'react-router-dom'
 
 const columns = [
   {
@@ -11,47 +15,60 @@ const columns = [
     dataIndex: 'name'
   },
   {
-    title: 'Product',
-    dataIndex: 'product'
+    title: 'Email',
+    dataIndex: 'email'
+  },
+  {
+    title: 'Mobile',
+    dataIndex: 'mobile'
   },
   {
     title: 'Status',
     dataIndex: 'status'
-  }
-]
-const data1 = [
-  {
-    key: '1',
-    name: 'John Brown',
-    product: 32,
-    status: 'New York No. 1 Lake Park'
   },
   {
-    key: '2',
-    name: 'John Brown',
-    product: 32,
-    status: 'New York No. 1 Lake Park'
-  },
-  {
-    key: '3',
-    name: 'John Brown',
-    product: 32,
-    status: 'New York No. 1 Lake Park'
-  },
-  {
-    key: '4',
-    name: 'John Brown',
-    product: 32,
-    status: 'New York No. 1 Lake Park'
+    title: 'Action',
+    dataIndex: 'action'
   }
 ]
 
 const Enquires = () => {
+  const enqState = useSelector((state) => state.enquiry.enquiries)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getEnquiries())
+  }, [dispatch])
+
+  const data = []
+  for (let i = 0; i < enqState.length; i++) {
+    data.push({
+      key: i + 1,
+      name: enqState[i].name,
+      email: enqState[i].email,
+      mobile: enqState[i].mobile,
+      status: (
+        <>
+          <select className="form-control form-select">
+            <option>Set Option</option>
+          </select>
+        </>
+      ),
+      action: (
+        <>
+          <Link to="/" className="ms-3 fs-3 text-danger">
+            <AiFillDelete />
+          </Link>
+        </>
+      )
+    })
+  }
+
   return (
     <div>
       <h3 className="mb-4 title">Enquiries</h3>
       <div>
-        <Table columns={columns} dataSource={data1} />
+        <Table columns={columns} dataSource={data} />
       </div>
     </div>
   )
