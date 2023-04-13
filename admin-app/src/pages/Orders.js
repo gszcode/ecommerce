91a2cf087d85a19a334435306e203381e18d36c2
Gabriel <gszcode@gmail.com>
2023-04-12 23:24:08 -0300

    rederizado de ordenes

--- admin-app/src/pages/Orders.js
+++ admin-app/src/pages/Orders.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Table } from 'antd'
+import { useDispatch, useSelector } from 'react-redux'
+import { getOrders } from '../features/auth/authSlice'
 
 const columns = [
   {
@@ -19,39 +21,32 @@ const columns = [
     dataIndex: 'status'
   }
 ]
-const data1 = [
-  {
-    key: '1',
-    name: 'John Brown',
-    product: 32,
-    status: 'New York No. 1 Lake Park'
-  },
-  {
-    key: '2',
-    name: 'John Brown',
-    product: 32,
-    status: 'New York No. 1 Lake Park'
-  },
-  {
-    key: '3',
-    name: 'John Brown',
-    product: 32,
-    status: 'New York No. 1 Lake Park'
-  },
-  {
-    key: '4',
-    name: 'John Brown',
-    product: 32,
-    status: 'New York No. 1 Lake Park'
-  }
-]
 
 const Orders = () => {
+  const orderState = useSelector((state) => state.auth.orders)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getOrders())
+  }, [dispatch])
+
+  console.log(orderState)
+
+  const data = []
+  // for (let i = 0; i < orderState.length; i++) {
+  //   data.push({
+  //     key: '4',
+  //     name: 'John Brown',
+  //     product: 32,
+  //     status: 'New York No. 1 Lake Park'
+  //   })
+  // }
+
   return (
     <div>
       <h3 className="mb-4 title">Orders</h3>
       <div>
-        <Table columns={columns} dataSource={data1} />
+        <Table columns={columns} dataSource={data} />
       </div>
     </div>
   )
