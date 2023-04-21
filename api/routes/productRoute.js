5cdb2e800ca2670ccb5f24107698ae101382c369
Gabriel <gszcode@gmail.com>
2023-04-21 03:09:04 -0300

    eliminacion de rutas de imagenes

--- api/routes/productRoute.js
+++ api/routes/productRoute.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const { authMiddleware, isAdmin } = require('../middlewares/authMiddleware')
-const { uploadPhoto, productImgResize } = require('../middlewares/uploadImages')
+
 const {
   createProduct,
   getProduct,
@@ -9,26 +9,16 @@ const {
   updateProduct,
   deleteProduct,
   addToWishList,
-  rating,
-  uploadImages,
-  deleteImages
+  rating
 } = require('../controllers/productCtrl')
 
 router.post('/', authMiddleware, isAdmin, createProduct)
-router.put(
-  '/upload',
-  authMiddleware,
-  isAdmin,
-  uploadPhoto.array('images', 10),
-  productImgResize,
-  uploadImages
-)
 router.get('/:id', getProduct)
 router.put('/wishlist', authMiddleware, addToWishList)
 router.put('/rating', authMiddleware, rating)
 router.put('/:id', authMiddleware, isAdmin, updateProduct)
 router.delete('/:id', authMiddleware, isAdmin, deleteProduct)
-router.delete('/delete-img/:id', authMiddleware, isAdmin, deleteImages)
+
 router.get('/', getAllProducts)
 
 module.exports = router
