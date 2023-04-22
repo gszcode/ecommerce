6b125a80f22f5135607637ff2fb163e547b13908
Gabriel <gszcode@gmail.com>
2023-04-22 03:48:43 -0300

    slice de crear producto creado

--- admin-app/src/features/product/productSlice.js
+++ admin-app/src/features/product/productSlice.js
@@ -20,6 +20,17 @@ export const getProducts = createAsyncThunk(
   }
 )
 
+export const createProducts = createAsyncThunk(
+  'product/create-products',
+  async (productData, thunkAPI) => {
+    try {
+      return await productService.createProduct(productData)
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error)
+    }
+  }
+)
+
 export const productSlice = createSlice({
   name: 'product',
   initialState,
@@ -35,6 +46,21 @@ export const productSlice = createSlice({
       state.products = action.payload
     })
     builder.addCase(getProducts.rejected, (state, action) => {
+      state.isLoading = false
+      state.isError = false
+      state.isSuccess = true
+      state.message = action.error
+    })
+    builder.addCase(createProducts.pending, (state) => {
+      state.isLoading = true
+    })
+    builder.addCase(createProducts.fulfilled, (state, action) => {
+      state.isLoading = false
+      state.isError = false
+      state.isSuccess = true
+      state.products = action.payload
+    })
+    builder.addCase(createProducts.rejected, (state, action) => {
       state.isLoading = false
       state.isError = false
       state.isSuccess = true
