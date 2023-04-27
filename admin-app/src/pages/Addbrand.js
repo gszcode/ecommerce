0ac399a34c954750f45618081a77052aba730ea8
Gabriel <gszcode@gmail.com>
2023-04-27 20:44:38 -0300

    creacion de marca con validaciones y envio de datos finalizado

--- admin-app/src/pages/Addbrand.js
+++ admin-app/src/pages/Addbrand.js
@@ -1,13 +1,58 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import CustomInput from '../components/CustomInput'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { useFormik } from 'formik'
+import { titleSchema } from '../utils/validations'
+import { createBrand } from '../features/brand/brandSlice'
+import { useStore } from '../hooks/useStore'
 
 const Addbrand = () => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const { isSuccess, isError, isLoading, createdBrand } = useStore().newBrand
+
+  useEffect(() => {
+    if (isSuccess && createdBrand) {
+      toast.success('Brand created Successfully!')
+    }
+    if (isError) {
+      toast.error('Something went Wrong!!')
+    }
+  }, [isSuccess, isError, isLoading, createdBrand])
+
+  const formik = useFormik({
+    initialValues: {
+      title: ''
+    },
+    validationSchema: titleSchema,
+    onSubmit: (values) => {
+      dispatch(createBrand(values))
+      formik.resetForm()
+
+      setTimeout(() => {
+        navigate('/admin/list-brand')
+      }, 3000)
+    }
+  })
+
   return (
     <div>
       <h3 className="mb-4 title">Add Brand</h3>
       <div>
-        <form>
-          <CustomInput type="text" label="Enter Brand" />
+        <form onSubmit={formik.handleSubmit}>
+          <CustomInput
+            type="text"
+            label="Enter Brand"
+            name="title"
+            onCh={formik.handleChange('title')}
+            onBlur={formik.handleBlur('title')}
+            val={formik.values.title}
+          />
+          <div className="error">
+            {formik.touched.title && formik.errors.title}
+          </div>
           <button
             className="btn btn-success border-0 rounded-3 my-5"
             type="submit"
