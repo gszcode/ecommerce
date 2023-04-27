3d60f98dc9d9ec16e4b89551206fb76c0907f275
Gabriel <gszcode@gmail.com>
2023-04-27 20:47:39 -0300

    slice de cracion de marca agregado

--- admin-app/src/features/brand/brandSlice.js
+++ admin-app/src/features/brand/brandSlice.js
@@ -20,26 +20,53 @@ export const getBrands = createAsyncThunk(
   }
 )
 
+export const createBrand = createAsyncThunk(
+  'brand/create-brand',
+  async (brandData, thunkAPI) => {
+    try {
+      return await brandService.createBrand(brandData)
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error)
+    }
+  }
+)
+
 export const brandSlice = createSlice({
   name: 'brand',
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(getBrands.pending, (state) => {
-      state.isLoading = true
-    })
-    builder.addCase(getBrands.fulfilled, (state, action) => {
-      state.isLoading = false
-      state.isError = false
-      state.isSuccess = true
-      state.brands = action.payload
-    })
-    builder.addCase(getBrands.rejected, (state, action) => {
-      state.isLoading = false
-      state.isError = true
-      state.isSuccess = false
-      state.message = action.error
-    })
+    builder
+      .addCase(getBrands.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(getBrands.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isError = false
+        state.isSuccess = true
+        state.brands = action.payload
+      })
+      .addCase(getBrands.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.isSuccess = false
+        state.message = action.error
+      })
+      .addCase(createBrand.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(createBrand.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isError = false
+        state.isSuccess = true
+        state.createdBrand = action.payload
+      })
+      .addCase(createBrand.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.isSuccess = false
+        state.message = action.error
+      })
   }
 })
 
