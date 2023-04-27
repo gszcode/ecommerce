84b3d8a3afca2c6fb47ed2dbfb86b83061b9f6be
Gabriel <gszcode@gmail.com>
2023-04-27 20:44:17 -0300

    creacion de categoria con validaciones y envio de datos finalizado

--- admin-app/src/pages/Addcat.js
+++ admin-app/src/pages/Addcat.js
@@ -1,13 +1,59 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import CustomInput from '../components/CustomInput'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { useFormik } from 'formik'
+import { titleSchema } from '../utils/validations'
+import { createCategory } from '../features/pcategory/pcategorySlice'
+import { useStore } from '../hooks/useStore'
 
 const Addcat = () => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const { isSuccess, isError, isLoading, createdCategory } =
+    useStore().newCategory
+
+  useEffect(() => {
+    if (isSuccess && createdCategory) {
+      toast.success('Category created Successfully!')
+    }
+    if (isError) {
+      toast.error('Something went Wrong!!')
+    }
+  }, [isSuccess, isError, isLoading, createdCategory])
+
+  const formik = useFormik({
+    initialValues: {
+      title: ''
+    },
+    validationSchema: titleSchema,
+    onSubmit: (values) => {
+      dispatch(createCategory(values))
+      formik.resetForm()
+
+      setTimeout(() => {
+        navigate('/admin/list-category')
+      }, 3000)
+    }
+  })
+
   return (
     <div>
       <h3 className="mb-4 title">Add Blog Category</h3>
       <div>
-        <form>
-          <CustomInput type="text" label="Enter Blog Category" />
+        <form onSubmit={formik.handleSubmit}>
+          <CustomInput
+            type="text"
+            label="Enter Product Category"
+            name="title"
+            onCh={formik.handleChange('title')}
+            onBlur={formik.handleBlur('title')}
+            val={formik.values.title}
+          />
+          <div className="error">
+            {formik.touched.title && formik.errors.title}
+          </div>
           <button
             className="btn btn-success border-0 rounded-3 my-5"
             type="submit"
