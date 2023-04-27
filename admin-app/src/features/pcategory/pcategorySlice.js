8779c183309cc837c94fc7527a64a487d13b6293
Gabriel <gszcode@gmail.com>
2023-04-27 20:47:20 -0300

    slice de cracion de categotia agregado

--- admin-app/src/features/pcategory/pcategorySlice.js
+++ admin-app/src/features/pcategory/pcategorySlice.js
@@ -20,26 +20,53 @@ export const getCategories = createAsyncThunk(
   }
 )
 
+export const createCategory = createAsyncThunk(
+  'productCategory/create-category',
+  async (categoryData, thunkAPI) => {
+    try {
+      return await pCategoryService.createCategory(categoryData)
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error)
+    }
+  }
+)
+
 export const pCategorySlice = createSlice({
   name: 'pCategories',
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(getCategories.pending, (state) => {
-      state.isLoading = true
-    })
-    builder.addCase(getCategories.fulfilled, (state, action) => {
-      state.isLoading = false
-      state.isError = false
-      state.isSuccess = true
-      state.pCategories = action.payload
-    })
-    builder.addCase(getCategories.rejected, (state, action) => {
-      state.isLoading = false
-      state.isError = true
-      state.isSuccess = false
-      state.message = action.error
-    })
+    builder
+      .addCase(getCategories.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(getCategories.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isError = false
+        state.isSuccess = true
+        state.pCategories = action.payload
+      })
+      .addCase(getCategories.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.isSuccess = false
+        state.message = action.error
+      })
+      .addCase(createCategory.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(createCategory.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isError = false
+        state.isSuccess = true
+        state.createdCategory = action.payload
+      })
+      .addCase(createCategory.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.isSuccess = false
+        state.message = action.error
+      })
   }
 })
 
