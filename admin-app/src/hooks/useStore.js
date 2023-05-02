c80f65fc76f658b2453ef64caa5ab5667be57b8a
Gabriel <gszcode@gmail.com>
2023-05-01 23:21:05 -0300

    llamado a la store de blog, blogcategoria, prodcategoria

--- admin-app/src/hooks/useStore.js
+++ admin-app/src/hooks/useStore.js
@@ -3,19 +3,27 @@ import { useSelector } from 'react-redux'
 export const useStore = () => {
   const brandState = useSelector((state) => state.brand.brands)
   const pCategoryState = useSelector((state) => state.pCategory.pCategories)
+  const bCategoryState = useSelector((state) => state.bCategory.bCategories)
   const colorState = useSelector((state) => state.color.colors)
   const imgState = useSelector((state) => state.upload.images)
   const newProduct = useSelector((state) => state.product)
   const newBrand = useSelector((state) => state.brand)
   const newCategory = useSelector((state) => state.pCategory)
+  const newColor = useSelector((state) => state.color)
+  const newBlogCategory = useSelector((state) => state.bCategory)
+  const blogState = useSelector((state) => state.blog)
 
   return {
     brandState,
     pCategoryState,
+    bCategoryState,
     colorState,
     imgState,
     newProduct,
     newBrand,
-    newCategory
+    newCategory,
+    newColor,
+    newBlogCategory,
+    blogState
   }
 }
