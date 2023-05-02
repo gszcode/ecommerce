2d1dd9b1a6458749f59053964baac9a92e38b4f7
Gabriel <gszcode@gmail.com>
2023-05-01 23:23:21 -0300

    servicio para agregar blogcategoria realizado

--- admin-app/src/features/bcategory/bcategoryService.js
+++ admin-app/src/features/bcategory/bcategoryService.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { base_url } from '../../utils/base_url'
+import { config } from '../../utils/axiosconfig'
 
 const getBlogCategory = async () => {
   const response = await axios(`${base_url}blogcategory/`)
@@ -7,6 +8,12 @@ const getBlogCategory = async () => {
   return response.data
 }
 
-const bCategoryService = { getBlogCategory }
+const createBlogCategory = async (blog) => {
+  const response = await axios.post(`${base_url}blogcategory/`, blog, config)
+
+  return response.data
+}
+
+const bCategoryService = { getBlogCategory, createBlogCategory }
 
 export default bCategoryService
