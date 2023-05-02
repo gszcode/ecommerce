5efe21d4671fc9d7e2d438d16cf30ce58228f0fc
Gabriel <gszcode@gmail.com>
2023-05-01 23:22:15 -0300

    funcion de agregar blogcategoria realizada

--- admin-app/src/features/bcategory/bcategorySlice.js
+++ admin-app/src/features/bcategory/bcategorySlice.js
@@ -22,6 +22,17 @@ export const getCategories = createAsyncThunk(
   }
 )
 
+export const createBlogCategory = createAsyncThunk(
+  'blogCategory/create-category',
+  async (blogData, thunkAPI) => {
+    try {
+      return await bCategoryService.createBlogCategory(blogData)
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error)
+    }
+  }
+)
+
 export const bCategorySlice = createSlice({
   name: 'bCategories',
   initialState,
@@ -38,6 +49,21 @@ export const bCategorySlice = createSlice({
         state.bCategories = action.payload
       })
       .addCase(getCategories.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.isSuccess = false
+        state.message = action.error
+      })
+      .addCase(createBlogCategory.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(createBlogCategory.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isError = false
+        state.isSuccess = true
+        state.createdBlogCategory = action.payload
+      })
+      .addCase(createBlogCategory.rejected, (state, action) => {
         state.isLoading = false
         state.isError = true
         state.isSuccess = false
